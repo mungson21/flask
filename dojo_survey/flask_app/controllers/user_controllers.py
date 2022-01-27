9f2de9b2f8f8f603a1ee062424cc8e731fb238f7
m ungs <ungson001@live.com>
2022-01-27 08:21:20 -0800

    assignment complete

--- dojo_survey/flask_app/controllers/user_controllers.py
+++ dojo_survey/flask_app/controllers/user_controllers.py
@@ -1,6 +1,8 @@
 from flask_app import app
 
-from flask import Flask, render_template, request, redirect, session
+from flask import render_template, request, redirect, session
+
+from flask_app.models.user_model import User
 
 
 @app.route('/')
@@ -10,8 +12,23 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     session['data']=request.form
-    return redirect('/result')
+    form_data = {
+        'name' : request.form['name'],
+        'location' : request.form['location'],
+        'language' : request.form['language'],
+        'comment' : request.form['comment']
+    }
+    valid = User.user_validation(form_data)
+    if valid:
+        results = User.create_user(form_data)
+        return redirect(f'/results/{results}')
+    return redirect('/')
 
-@app.route('/result')
-def result():
-    return render_template('results.html', user_name=session['data']['user_name'], dojo_location=session['data']['dojo_location'], fav_language=session['data']['fav_language'], comment=session['data']['comment'])
+@app.route('/results/<int:user_id>')
+def get_one(user_id):
+    data = {
+        'id' : user_id
+    }
+    user = User.get_one(data)
+    return render_template('results.html', user=user, name=session['data']['name'], location=session['data']['location'], language=session['data']['language'], comment=session['data']['comment'])
+
--- dojo_survey/flask_app/models/user_model.py
+++ dojo_survey/flask_app/models/user_model.py
@@ -1,3 +1,7 @@
+from flask import flash
+
+import re
+
 from flask_app.config.mysqlconnection import connectToMySQL
 
 db = 'dojo_survey'
@@ -11,3 +15,25 @@ class User:
         self.comment = data['comment']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @classmethod
+    def create_user(cls, data):
+        query = 'INSERT INTO dojos (name, location, language, comment) VALUES (%(name)s, %(location)s, %(language)s, %(comment)s);'
+        return connectToMySQL(db).query_db(query, data)
+
+    @classmethod
+    def get_one(cls, data):
+        query = " SELECT * FROM dojos WHERE id = %(id)s;"
+        results = connectToMySQL(db).query_db(query, data)
+        return cls(results[0])
+
+    @staticmethod
+    def user_validation(data):
+        is_valid = True
+        if len(data['name']) < 3:
+            flash('Name has to be longer than 3 characters')
+            is_valid = False
+        if len(data['comment']) < 10:
+            flash('Comments has to be longer than 10 characters')
+            is_valid = False
+        return is_valid
